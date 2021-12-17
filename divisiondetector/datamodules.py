d524c6d486a54a986f87c131189c623223084284
Nicholas-Ho <j.xiong@live.com>
2021-12-17 13:19:12 +0000

    Added Datasets and Datamodules

--- divisiondetector/datamodules.py
+++ divisiondetector/datamodules.py
@@ -5,20 +5,43 @@ from divisiondetector.datasets import DivisionDataset
 
 class DivisionDataModule(pl.LightningDataModule):
 
-    def __init__(self,
-                 batch_size,
-                 loader_workers=10):
-
+    def __init__(self, batch_size, loader_workers=10):
         super().__init__()
+
         self.batch_size = batch_size
         self.loader_workers = loader_workers
 
     def setup(self, stage=None):
+        window_size = (500, 500, 500)
+        time_window = (1, 1)
+        mode = 'ball'
+        ball_radius = (10, 10, 10)
 
         # TODO: create different train validataion and test datasets
-        self.ds_train = DivisionDataset()
-        self.ds_val = DivisionDataset()
-        self.ds_test = DivisionDataset()
+        self.ds_train = DivisionDataset(
+            "16-10-13-EGG_manual_annotations_t10_t103-t107_t186-t190-vertices.csv",
+            "volumes.zarr",
+            window_size,
+            time_window,
+            mode,
+            ball_radius
+        )
+        self.ds_val = DivisionDataset(
+            "16-10-13-EGG_manual_annotations_t10_t103-t107_t186-t190-vertices.csv",
+            "volumes.zarr",
+            window_size,
+            time_window,
+            mode,
+            ball_radius
+        )
+        self.ds_test = DivisionDataset(
+            "16-10-13-EGG_manual_annotations_t10_t103-t107_t186-t190-vertices.csv",
+            "volumes.zarr",
+            window_size,
+            time_window,
+            mode,
+            ball_radius
+        )
 
     def train_dataloader(self):
         return DataLoader(self.ds_train,
